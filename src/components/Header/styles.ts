9b4abd62db209f2031765ef28ef6c912664eb241
Renan Floriano Neves <renanfneves@gmail.com>
2023-03-29 00:05:31 +0100

    [FIRST COMMIT]: setup project

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import heroImage from '../../assets/hero.svg'
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 18.5rem;
+  background: url(${heroImage}) no-repeat center;
+  background-size: cover;
+`
---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  height: 100vh;
+  align-items: center;
+
+  display: flex;
+  flex-direction: column;
+`
+export const OutletContainer = styled.div`
+  margin-top: -13rem;
+  max-width: 54rem;
+`
